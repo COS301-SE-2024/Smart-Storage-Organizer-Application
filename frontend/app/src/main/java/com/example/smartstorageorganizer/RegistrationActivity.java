a6d0ce2dc4eb8d1b14a4f241145ae365cc57dcf2
Segadimane-Makau <161027953+Segadimane-Makau@users.noreply.github.com>
2024-05-20 23:24:42 +0200

    Registration ui done

--- frontend/app/src/main/java/com/example/smartstorageorganizer/RegistrationActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/RegistrationActivity.java
@@ -1,9 +1,6 @@
 package com.example.smartstorageorganizer;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        TextView signUpLink = findViewById(R.id.signUpLink);
+        setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        signUpLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
