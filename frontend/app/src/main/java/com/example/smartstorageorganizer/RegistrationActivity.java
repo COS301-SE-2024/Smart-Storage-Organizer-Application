7fa971251f0556f1f6e2384acc5e8b1e28ee0c95
Segadimane-Makau <161027953+Segadimane-Makau@users.noreply.github.com>
2024-05-22 11:24:59 +0200

    Added a function to check for form validation.

--- frontend/app/src/main/java/com/example/smartstorageorganizer/RegistrationActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/RegistrationActivity.java
@@ -1,6 +1,10 @@
 package com.example.smartstorageorganizer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,20 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
+        RelativeLayout registerButton = findViewById(R.id.buttonRegister);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
