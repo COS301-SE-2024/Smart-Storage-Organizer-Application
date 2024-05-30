96a643269f855a05421d891ca301503258eaead3
Segadimane-Makau <161027953+Segadimane-Makau@users.noreply.github.com>
2024-05-30 18:23:59 +0200

    Authentication (#18)
    * Updated SIgnUp
    
    * Amplify Import Fix
    
    * MyAmplifyApp Added
    
    * Updated Functions
    
    * SignUp Works
    
    * Confirm SignUp WOrks
    
    * SignIn Works
    
    * Updated Login attempted to return something
    
    * Authentication and Authorization is ready for integregation
    
    * Updated
    
    * Added  AuthSignResult
    
    * Login, Registration and Email Verification Updated.
    
    * Added the IsSignedIn function to check if the user is already signed in
    
    * Updated the logout and login
    
    * handled the user not verified exception
    
    * Edit MyAmplifyAPp  to include API
    
    * created the resendOtp verification
    
    * created the resendOtp verification
    
    * Update ReadMe.md
    
    added the aws pull request
    
    ---------
    
    Co-authored-by: Siya2-0 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
    Co-authored-by: VICTOR ZHOU <110187483+Xerof47Vel@users.noreply.github.com>

---
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/MyAmplifyApp.java
@@ -0,0 +1,28 @@
+package com.example.smartstorageorganizer;
+import android.app.Application;
+import android.util.Log;
+
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+//import com.amplifyframework.core.configuration.AmplifyConfiguration;
+
+public class MyAmplifyApp extends Application {
+    private AmplifyException error;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify " +error);
+        }
+    }
+}
--- frontend/app/src/main/java/com/example/smartstorageorganizer/ProfileManagementActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/ProfileManagementActivity.java
@@ -2,8 +2,11 @@ package com.example.smartstorageorganizer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.amplifyframework.auth.cognito.result.AWSCognitoAuthSignOutResult;
+import com.amplifyframework.core.Amplify;
 
 public class ProfileManagementActivity extends AppCompatActivity {
 
@@ -24,12 +30,15 @@ public class ProfileManagementActivity extends AppCompatActivity {
         AppCompatButton editProfileButton = findViewById(R.id.editProfileButton);
         ImageView ProfileBackButton = findViewById(R.id.ProfileBackButton);
         ConstraintLayout logoutButton = findViewById(R.id.logoutButton);
+        TextView email = findViewById(R.id.email);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        email.setText(getIntent().getStringExtra("email"));
 
         ProfileBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,9 +50,7 @@ public class ProfileManagementActivity extends AppCompatActivity {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ProfileManagementActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                SignOut();
             }
         });
 
@@ -54,5 +61,43 @@ public class ProfileManagementActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void SignOut()
+    {
+        Amplify.Auth.signOut(signOutResult -> {
+            if (signOutResult instanceof AWSCognitoAuthSignOutResult.CompleteSignOut) {
+                // Sign Out completed fully and without errors.
+                Log.i("AuthQuickStart", "Signed out successfully");
+                //move to a different page
+                runOnUiThread(() -> {
+                    Intent intent = new Intent(ProfileManagementActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                });
+            } else if (signOutResult instanceof AWSCognitoAuthSignOutResult.PartialSignOut) {
+                // Sign Out completed with some errors. User is signed out of the device.
+                AWSCognitoAuthSignOutResult.PartialSignOut partialSignOutResult =
+                        (AWSCognitoAuthSignOutResult.PartialSignOut) signOutResult;
+                //move to the different page
+                runOnUiThread(() -> {
+                    Intent intent = new Intent(ProfileManagementActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                });
+
+            } else if (signOutResult instanceof AWSCognitoAuthSignOutResult.FailedSignOut) {
+                AWSCognitoAuthSignOutResult.FailedSignOut failedSignOutResult =
+                        (AWSCognitoAuthSignOutResult.FailedSignOut) signOutResult;
+                // Sign Out failed with an exception, leaving the user signed in.
+                Log.e("AuthQuickStart", "Sign out Failed", failedSignOutResult.getException());
+                //dont move to different page
+                runOnUiThread(() -> {
+                    Toast.makeText(this, "Sign Out Failed.", Toast.LENGTH_LONG).show();
+
+                });
+            }
+        });
+
     }
 }
