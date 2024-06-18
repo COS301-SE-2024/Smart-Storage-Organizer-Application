8fdccda3ac5d2323e55c2cca47faac8f9c49845b
Siyabonga Mbuyisa-20491621 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
2024-06-18 20:42:27 +0200

    Updated FetchCategory and AddCategory

--- frontend/app/src/main/java/com/example/smartstorageorganizer/HomeActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/HomeActivity.java
@@ -205,7 +205,7 @@ public class HomeActivity extends AppCompatActivity {
         
         MediaType JSON = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        String API_URL = "https://m1bavqqu90.execute-api.eu-north-1.amazonaws.com/deployment/ssrest/SearchByEmail";
+        String API_URL = "https://m1bavqqu90.execute-api.eu-north-1.amazonaws.com/deployment/ssrest/AddCategory";
         RequestBody body = RequestBody.create(json, JSON);
 
         Request request = new Request.Builder()
@@ -221,7 +221,7 @@ public class HomeActivity extends AppCompatActivity {
 
         MediaType JSON = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        String API_URL = "https://m1bavqqu90.execute-api.eu-north-1.amazonaws.com/deployment/ssrest/SearchByEmail";
+        String API_URL = "https://m1bavqqu90.execute-api.eu-north-1.amazonaws.com/deployment/ssrest/AddCategory";
         RequestBody body = RequestBody.create(json, JSON);
 
         Request request = new Request.Builder()
