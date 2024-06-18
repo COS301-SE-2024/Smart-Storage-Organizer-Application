bd7d5af6444fb541bb0ceca44844883225b350f6
Segadimane-Makau <161027953+Segadimane-Makau@users.noreply.github.com>
2024-06-18 22:18:35 +0200

    fetching of categories now working.

--- frontend/app/src/main/java/com/example/smartstorageorganizer/AddCategoryActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/AddCategoryActivity.java
@@ -1,6 +1,7 @@
 package com.example.smartstorageorganizer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,11 +12,29 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.smartstorageorganizer.model.ItemModel;
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class AddCategoryActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
@@ -23,31 +42,37 @@ public class AddCategoryActivity extends AppCompatActivity {
     private TextInputLayout subcategoryInput;
     private TextInputLayout parentCategoryInput;
 
+    private List<String> parentCategories = new ArrayList<>();
+    private List<String> parentCategoriesIcons = new ArrayList<>();
+    private Spinner mySpinner;
+    private ConstraintLayout addButton;
+    private boolean flag = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_category);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Spinner mySpinner = findViewById(R.id.mySpinner);
+            mySpinner = findViewById(R.id.mySpinner);
             radioGroup = findViewById(R.id.radioGroup);
             subcategoryInput = findViewById(R.id.subcategoryInput);
             parentCategoryInput = findViewById(R.id.parentcategoryInput);
+            addButton = findViewById(R.id.addButton);
+
+            if(flag) {
+                FetchCategory(0, "ezemakau@gmail.com");
+            }
 
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                    R.array.dropdown_items, android.R.layout.simple_spinner_item);
-
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-            // Apply the adapter to the spinner
-            mySpinner.setAdapter(adapter);
-
-            // Set a listener for the spinner
-
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AddCategory(0, "Gaming", "ezemakau@gmail.com");
+                }
+            });
             radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -84,5 +109,121 @@ public class AddCategoryActivity extends AppCompatActivity {
 
             return insets;
         });
+    }
+
+    public void FetchCategory(int ParentCategory, String email)
+    {
+        if(flag) {
+            String json = "{\"useremail\":\""+email+"\", \"parentcategory\":\""+Integer.toString(ParentCategory)+"\" }";
+
+
+            MediaType JSON = MediaType.get("application/json; charset=utf-8");
+            OkHttpClient client = new OkHttpClient();
+            String API_URL = "https://m1bavqqu90.execute-api.eu-north-1.amazonaws.com/deployment/ssrest/FetchCategory";
+            RequestBody body = RequestBody.create(json, JSON);
+
+            Request request = new Request.Builder()
+                    .url(API_URL)
+                    .post(body)
+                    .build();
+
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                    runOnUiThread(() -> Log.e("Category Request Method", "GET request failed", e));
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        final String responseData = response.body().string();
+                        runOnUiThread(() -> {
+                            runOnUiThread(() -> Log.e("Category Response Results", responseData));
+
+                            try {
+                                JSONObject jsonObject = new JSONObject(responseData);
+
+                                int statusCode = jsonObject.getInt("statusCode");
+                                String status = jsonObject.getString("status");
+
+                                String bodyString = jsonObject.getString("body");
+                                JSONArray bodyArray = new JSONArray(bodyString);
+
+                                List<ItemModel> items = new ArrayList<>();
+
+                                runOnUiThread(() -> Log.e("Category Details Array", bodyArray.toString()));
+
+                                for (int i = 0; i < bodyArray.length(); i++) {
+                                    JSONObject itemObject = bodyArray.getJSONObject(i);
+                                    parentCategories.add(itemObject.getString("categoryname"));
+
+                                    String temp = itemObject.getString("categoryname");
+
+                                    runOnUiThread(() -> Log.e("Category Details", temp));
+                                }
+//                            fillSpinner();
+                                runOnUiThread(() -> {
+                                    ArrayAdapter<String> adapter = new ArrayAdapter<>(AddCategoryActivity.this, android.R.layout.simple_spinner_item, parentCategories);
+                                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                    mySpinner.setAdapter(adapter);
+                                });
+
+
+//                            requireActivity().runOnUiThread(() -> {
+//                                fetchItemsLoader.setVisibility(View.GONE);
+//                                itemRecyclerView.setVisibility(View.VISIBLE);
+//                            });
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        });
+
+                    } else {
+                        runOnUiThread(() -> Log.e("Request Method", "GET request failed: " + response));
+                    }
+                }
+            });
+            flag = false;
+        }
+
+    }
+
+    public void AddCategory(int ParentCategory, String CategoryName, String email)
+    {
+        String json = "{\"useremail\":\""+email+"\", \"parentcategory\":\""+Integer.toString(ParentCategory)+"\", \"categoryname\":\""+CategoryName+"\" }";
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        String API_URL = "https://m1bavqqu90.execute-api.eu-north-1.amazonaws.com/deployment/ssrest/AddCategory";
+        RequestBody body = RequestBody.create(json, JSON);
+
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(() -> Log.e("Request Method", "POST request failed", e));
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String responseData = response.body().string();
+                    runOnUiThread(() -> {
+                        runOnUiThread(() -> Log.i("Request Method", "POST request succeeded: " + responseData));
+                    });
+                } else {
+                    runOnUiThread(() -> Log.e("Request Method", "POST request failed: " + response.code()));
+                }
+            }
+        });
+
     }
 }
