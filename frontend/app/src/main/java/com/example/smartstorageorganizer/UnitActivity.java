4793519004e4cedd3e34caf0120936d42edde4fd
Segadimane-Makau <161027953+Segadimane-Makau@users.noreply.github.com>
2024-06-20 20:57:17 +0200

    You can now fetch parent categories on the unit page

--- frontend/app/src/main/java/com/example/smartstorageorganizer/UnitActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/UnitActivity.java
@@ -1,6 +1,8 @@
 package com.example.smartstorageorganizer;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
@@ -10,9 +12,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.smartstorageorganizer.model.CategoryModel;
+import com.example.smartstorageorganizer.model.ItemModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class UnitActivity extends AppCompatActivity {
     String[] categories = {"Category 1", "Category 2", "Category 3", "Category 4", "Category 5", "Category 6", "Category 7", "Category 8"};
     boolean flag = true;
+    public ArrayList<CategoryModel> categoryModelList = new ArrayList<>();
+    private List<String> parentCategories = new ArrayList<>();
+    private LinearLayout checkboxContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +45,11 @@ public class UnitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_unit);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 
-            LinearLayout checkboxContainer = findViewById(R.id.checkbox_container);
+            checkboxContainer = findViewById(R.id.checkbox_container);
 
             if(flag){
-                for (String category : categories) {
-                    CheckBox checkBox = new CheckBox(this);
-                    checkBox.setText(category);
-                    checkboxContainer.addView(checkBox);
-                }
-                flag = false;
+                FetchCategory(0, "ezemakau@gmail.com");
+//                flag = false;
             }
 
             // Create checkboxes dynamically and add them to the LinearLayout
@@ -38,5 +58,97 @@ public class UnitActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void FetchCategory(int ParentCategory, String email)
+    {
+        if(flag) {
+            String json = "{\"useremail\":\""+email+"\", \"parentcategory\":\""+Integer.toString(ParentCategory)+"\" }";
+
+
+            MediaType JSON = MediaType.get("application/json; charset=utf-8");
+            OkHttpClient client = new OkHttpClient();
+            String API_URL = BuildConfig.FetchCategoryEndPoint;
+            RequestBody body = RequestBody.create(json, JSON);
+
+            Request request = new Request.Builder()
+                    .url(API_URL)
+                    .post(body)
+                    .build();
+
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                    runOnUiThread(() -> Log.e("Category Request Method", "GET request failed", e));
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        final String responseData = response.body().string();
+                        runOnUiThread(() -> {
+                            runOnUiThread(() -> Log.e("Category Response Results", responseData));
+
+                            try {
+                                JSONObject jsonObject = new JSONObject(responseData);
+
+                                int statusCode = jsonObject.getInt("statusCode");
+                                String status = jsonObject.getString("status");
+
+                                String bodyString = jsonObject.getString("body");
+                                JSONArray bodyArray = new JSONArray(bodyString);
+
+                                List<ItemModel> items = new ArrayList<>();
+
+                                runOnUiThread(() -> Log.e("Category Details Array", bodyArray.toString()));
+                                parentCategories.add("");
+                                for (int i = 0; i < bodyArray.length(); i++) {
+                                    JSONObject itemObject = bodyArray.getJSONObject(i);
+
+                                    CategoryModel parentCategory = new CategoryModel();
+                                    parentCategory.setCategoryID(itemObject.getString("id"));
+                                    parentCategory.setCategoryName(itemObject.getString("categoryname"));
+                                    parentCategory.setImageUrl(itemObject.getString("icon"));
+
+                                    categoryModelList.add(parentCategory);
+//                                    categoryAdapter.notifyDataSetChanged();
+
+                                    Log.i("Category Image Url", itemObject.getString("icon"));
+                                    parentCategories.add(itemObject.getString("categoryname"));
+
+
+                                }
+                                runOnUiThread(() -> {
+                                    for (CategoryModel category : categoryModelList) {
+                                        CheckBox checkBox = new CheckBox(UnitActivity.this);
+                                        checkBox.setText(category.getCategoryName());
+                                        checkboxContainer.addView(checkBox);
+                                    }
+//                                    ArrayAdapter<String> adapter = new ArrayAdapter<>(UnitActivity.this, android.R.layout.simple_spinner_item, parentCategories);
+//                                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                                    mySpinner.setAdapter(adapter);
+                                });
+
+
+//                            requireActivity().runOnUiThread(() -> {
+//                                fetchItemsLoader.setVisibility(View.GONE);
+//                                itemRecyclerView.setVisibility(View.VISIBLE);
+//                            });
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        });
+
+                    } else {
+                        runOnUiThread(() -> Log.e("Request Method", "GET request failed: " + response));
+                    }
+                }
+            });
+            flag = false;
+        }
+
     }
 }
