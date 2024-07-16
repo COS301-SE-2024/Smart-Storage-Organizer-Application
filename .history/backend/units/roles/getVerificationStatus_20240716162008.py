bad9424cbee4144af4691e5616b5dc108fef9dcb
VICTOR ZHOU <110187483+Xerof47Vel@users.noreply.github.com>
2024-07-16 17:07:01 +0200

    setUserToUnverified Endpoint

---
+++ .history/backend/units/roles/getVerificationStatus_20240716162008.py
@@ -0,0 +1,21 @@
+import os
+import boto3
+
+def lambda_handler(event, context):
+    client = boto3.client('cognito-idp')
+    response = client.admin_list_groups_for_user(
+        UserPoolId=os.environ['USER_POOL_ID'],
+        Username=event['body']['username']
+    )
+    groups=response['Groups']
+    for group in groups:
+        if group['GroupName']=='verifiedUsers':
+            return {
+                'statusCode': 200,
+                'body': 'verified'
+            }
+    return {
+        'statusCode': 200,
+        'body': 'unverified'
+    }
+
