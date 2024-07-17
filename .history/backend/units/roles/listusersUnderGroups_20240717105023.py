2039a6b00693892b8a04910b2ad620aaa034ab9b
VICTOR ZHOU <110187483+Xerof47Vel@users.noreply.github.com>
2024-07-17 10:53:59 +0200

    added the listUsersUnder a group endpoint

---
+++ .history/backend/units/roles/listusersUnderGroups_20240717105023.py
@@ -0,0 +1,39 @@
+import boto3
+import os
+
+def lambda_handler(event, context):
+    client=boto3.client('cognito-idp')
+    response=client.admin_list_groups_for_user(
+        UserPoolId='us-east-1_EPbgIUMEQ',
+        Username=event['body']['username']
+    )
+    groups=response['Groups']
+    for  group in groups:
+        if group['GroupName']=='Manager':
+            try:
+                response=client.list_users_in_group(
+                    UserPoolId='us-east-1_EPbgIUMEQ',
+                    GroupName=event['body']['type']
+                )
+                return {
+                    'statusCode': 200,
+                    'body': response['Users']
+                }
+            except client.exceptions.ClientError as error:
+                return {
+                    'statusCode': 500,
+                    'body': str(error)
+                }
+    return {
+        "statusCode": 403,
+        "body": 'User is not a Manager'
+    }
+
+event={
+    "body": {
+        "username": "zhouvel7@gmail.com",
+        "type": "normalUsers"
+    }
+}
+context={}
+print(lambda_handler(event, context))
---
+++ backend/units/roles/listusersUnderGroups.py
@@ -0,0 +1,39 @@
+import boto3
+import os
+
+def lambda_handler(event, context):
+    client=boto3.client('cognito-idp')
+    response=client.admin_list_groups_for_user(
+        UserPoolId=os.environ['USER_POOL_ID'],
+        Username=event['body']['username']
+    )
+    groups=response['Groups']
+    for  group in groups:
+        if group['GroupName']=='Manager':
+            try:
+                response=client.list_users_in_group(
+                    UserPoolId=os.environ['USER_POOL_ID'],
+                    GroupName=event['body']['type']
+                )
+                return {
+                    'statusCode': 200,
+                    'body': response['Users']
+                }
+            except client.exceptions.ClientError as error:
+                return {
+                    'statusCode': 500,
+                    'body': str(error)
+                }
+    return {
+        "statusCode": 403,
+        "body": 'User is not a Manager'
+    }
+
+event={
+    "body": {
+        "username": "zhouvel7@gmail.com",
+        "type": "Admin"
+    }
+}
+context={}
+print(lambda_handler(event, context))
