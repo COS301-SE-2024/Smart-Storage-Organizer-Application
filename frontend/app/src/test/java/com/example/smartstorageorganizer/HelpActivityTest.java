71a3c281e2b5b7e586fac4e2a63ecab934889cb9
Tshegofatso Mapheto <u20735929@tuks.co.za>
2024-07-26 17:29:21 +0200

    HelpActivity UnitTest Done

---
+++ frontend/app/src/test/java/com/example/smartstorageorganizer/HelpActivityTest.java
@@ -0,0 +1,56 @@
+package com.example.smartstorageorganizer;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static java.util.concurrent.CompletableFuture.allOf;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.CountDownTimer;
+
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowLooper;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.P)
+public class HelpActivityTest {
+
+    HelpActivity Page;
+    @Mock
+    HelpActivity MockedPage = mock(HelpActivity.class);
+
+    @Before
+    public void setup() {
+        Page = Robolectric.buildActivity(HelpActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void shouldNotBeNull() {
+        assertNotNull(Page);
+    }
+}
