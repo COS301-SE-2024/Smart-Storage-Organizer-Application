f1fc767003779e7d726487b82a214d1a2b668dfa
Mosa Letswalo <letswalomosa@tuks.co.za>
2024-08-05 23:05:47 +0200

    FCM: FirebaseMessagingService file

---
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/FirebaseMessagingService.java
@@ -0,0 +1,39 @@
+package com.example.smartstorageorganizer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+//import com.google.firebase.messaging.FirebaseMessagingService;
+//import com.amazonaws.mobile.client.results.Tokens;
+//import com.google.firebase.messaging.RemoteMessage;
+//
+//import com.amazonaws.mobile.client.AWSMobileClient;
+//import com.amazonaws.mobile.client.Callback;
+//import com.amazonaws.mobile.client.results.SignInResult;
+//
+//import org.json.JSONObject;
+//
+//import java.io.OutputStream;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+
+public class FirebaseMessagingService extends Service {
+    public FirebaseMessagingService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //Implementation
+}
