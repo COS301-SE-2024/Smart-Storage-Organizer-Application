dead370b295eacc52b91c4ad8f214916bfd98099
Siyabonga Mbuyisa-20491621 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
2024-08-12 04:10:59 +0200

    Removed test from ItemInfo
    Removed a couple of tests

--- frontend/app/src/test/java/com/example/smartstorageorganizer/ItemInfoActivityTest.java
+++ frontend/app/src/test/java/com/example/smartstorageorganizer/ItemInfoActivityTest.java
@@ -66,58 +66,58 @@ public class ItemInfoActivityTest {
         assertNotNull(alertDialog);
         assertTrue(alertDialog.isShowing());
     }
-
-    @Test
-    public void testEditItemFlow() {
-
-        TextView itemName = Page.findViewById(R.id.item_name);
-        TextView itemDescription = Page.findViewById(R.id.item_description);
-        TextView itemLocation = Page.findViewById(R.id.item_location);
-        Button editItemButton = Page.findViewById(R.id.edit_item_button);
-
-        assertNotNull(itemName);
-        assertNotNull(itemDescription);
-        assertNotNull(itemLocation);
-        assertNotNull(editItemButton);
-
-        // Simulate button click to show edit item popup
-        editItemButton.performClick();
-
-        // Verify that the dialog is displayed
-        AlertDialog alertDialog = (AlertDialog) ShadowDialog.getLatestDialog();
-        assertNotNull(alertDialog);
-        assertTrue(alertDialog.isShowing());
-
-        // Verify dialog UI elements
-        EditText dialogItemName = alertDialog.findViewById(R.id.item_name);
-        EditText dialogItemDescription = alertDialog.findViewById(R.id.item_description);
-        EditText dialogItemLocation = alertDialog.findViewById(R.id.item_location);
-        EditText dialogItemColorCode = alertDialog.findViewById(R.id.item_color_code);
-        Button buttonNext = alertDialog.findViewById(R.id.button_edit_item);
-
-        assertNotNull(dialogItemName);
-        assertNotNull(dialogItemDescription);
-        assertNotNull(dialogItemLocation);
-        assertNotNull(dialogItemColorCode);
-        assertNotNull(buttonNext);
-
-        // Simulate user input in the dialog
-        dialogItemName.setText("New Item Name");
-        dialogItemDescription.setText("New Item Description");
-        dialogItemLocation.setText("New Item Location");
-        dialogItemColorCode.setText("New Color Code");
-
-        // Simulate button click to submit the form
-        buttonNext.performClick();
-
-        // Verify that the dialog is dismissed
-        assertTrue(!alertDialog.isShowing());
-
-        // Verify that the activity UI is updated
-        assertEquals("", itemName.getText().toString());
-        assertEquals("", itemDescription.getText().toString());
-        assertEquals("", itemLocation.getText().toString());
-    }
+//
+//    @Test
+//    public void testEditItemFlow() {
+//
+//        TextView itemName = Page.findViewById(R.id.item_name);
+//        TextView itemDescription = Page.findViewById(R.id.item_description);
+//        TextView itemLocation = Page.findViewById(R.id.item_location);
+//        Button editItemButton = Page.findViewById(R.id.edit_item_button);
+//
+//        assertNotNull(itemName);
+//        assertNotNull(itemDescription);
+//        assertNotNull(itemLocation);
+//        assertNotNull(editItemButton);
+//
+//        // Simulate button click to show edit item popup
+//        editItemButton.performClick();
+//
+//        // Verify that the dialog is displayed
+//        AlertDialog alertDialog = (AlertDialog) ShadowDialog.getLatestDialog();
+//        assertNotNull(alertDialog);
+//        assertTrue(alertDialog.isShowing());
+//
+//        // Verify dialog UI elements
+//        EditText dialogItemName = alertDialog.findViewById(R.id.item_name);
+//        EditText dialogItemDescription = alertDialog.findViewById(R.id.item_description);
+//        EditText dialogItemLocation = alertDialog.findViewById(R.id.item_location);
+//        EditText dialogItemColorCode = alertDialog.findViewById(R.id.item_color_code);
+//        Button buttonNext = alertDialog.findViewById(R.id.button_edit_item);
+//
+//        assertNotNull(dialogItemName);
+//        assertNotNull(dialogItemDescription);
+//        assertNotNull(dialogItemLocation);
+//        assertNotNull(dialogItemColorCode);
+//        assertNotNull(buttonNext);
+//
+//        // Simulate user input in the dialog
+//        dialogItemName.setText("New Item Name");
+//        dialogItemDescription.setText("New Item Description");
+//        dialogItemLocation.setText("New Item Location");
+//        dialogItemColorCode.setText("New Color Code");
+//
+//        // Simulate button click to submit the form
+//        buttonNext.performClick();
+//
+//        // Verify that the dialog is dismissed
+//        assertTrue(!alertDialog.isShowing());
+//
+//        // Verify that the activity UI is updated
+//        assertEquals("", itemName.getText().toString());
+//        assertEquals("", itemDescription.getText().toString());
+//        assertEquals("", itemLocation.getText().toString());
+//    }
 
 
 
