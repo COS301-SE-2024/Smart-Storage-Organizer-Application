1fc92c9c835e2f1174cafe41822beff64f80ea38
Mosa Letswalo <letswalomosa@tuks.co.za>
2024-08-21 12:52:47 +0200

    FCM::update

--- frontend/app/src/main/java/com/example/smartstorageorganizer/FirebaseMessagingService.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/FirebaseMessagingService.java
@@ -1,23 +1,20 @@
 package com.example.smartstorageorganizer;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
-import com.amazonaws.mobile.client.results.Tokens;
-import com.google.firebase.messaging.RemoteMessage;
 
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.Callback;
-import com.amazonaws.mobile.client.results.SignInResult;
+import com.amazonaws.mobile.client.results.Tokens;
+import com.google.firebase.messaging.RemoteMessage;
 
 import org.json.JSONObject;
 
@@ -26,6 +23,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    // Constructor
+    public FirebaseMessagingService() {
+        Log.i("FCMService", "FirebaseMessagingService initialized.");
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("FCMService", "onCreate called in FirebaseMessagingService.");
+    }
 
 
     //Implementation
