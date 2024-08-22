de9bfd3a90e9f5e20c7419b86a2b7c410b869256
Mosa Letswalo <letswalomosa@tuks.co.za>
2024-08-22 19:31:33 +0200

    NotificationBody file

---
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/NotificationBody.java
@@ -0,0 +1,27 @@
+package com.example.smartstorageorganizer;
+
+public class NotificationBody {
+    private String title;
+    private String body;
+
+    public NotificationBody(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
