38a6943df1608fdfc623b131223c9af6da00dad5
VICTOR ZHOU <110187483+Xerof47Vel@users.noreply.github.com>
2024-09-03 17:56:00 +0200

    Update listUsersUnderGroup.py

--- backend/listUsersUnderGroup.py
+++ backend/listUsersUnderGroup.py
@@ -1,12 +1,13 @@
 import boto3
 import os
 import json
-
+import datetime
 def lambda_handler(event, context):
     client=boto3.client('cognito-idp')
     try:
-        response=client.list_users(
-            UserPoolId=os.environ['USER_POOL_ID']
+        response = client.list_users_in_group(
+            UserPoolId='us-east-1_EPbgIUMEQ',
+            GroupName=event['body']['Group'],
         )
     except client.exceptions.ClientError as error:
         return {
@@ -15,10 +16,20 @@ def lambda_handler(event, context):
         }
     users=response['Users']
     filtered_users=[]
-    if event['body']['type']=='Admin':
+    if event['body']['AccessLevel']=='Admin':
         for user in users:
-           if user['Attributes']['address']==event['body']['organization_id']:
-                filtered_users.append(user)
+           for attribute in user['Attributes']:
+                if attribute['Name'] == 'address' and attribute['Value'] == event['body']['organization_id']:
+                    user_info = {}
+                    for attr in user['Attributes']:
+                        if attr['Name'] in ['email', 'name', 'family_name']:
+                            user_info[attr['Name']] = attr['Value']
+                    filtered_users.append(user_info)
+                    break
+        for user in filtered_users:
+            for key, value in user.items():
+                if isinstance(value, datetime.datetime):
+                    user[key] = value.isoformat()
         return {
             'statusCode': 200,
             'body': filtered_users
@@ -27,3 +38,9 @@ def lambda_handler(event, context):
         "statusCode": 403,
         "body": 'User is not a Manager'
     }
+
+event={'body': {'AccessLevel': 'Admin',
+                'Group':'Admin',
+                'organization_id': 'Makro'}}
+context={}
+print(lambda_handler(event, context))
