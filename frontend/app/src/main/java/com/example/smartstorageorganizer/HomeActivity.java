bd258130c45be56868460f937a77e94c380abb9f
Segadimane-Makau <161027953+Segadimane-Makau@users.noreply.github.com>
2024-09-17 10:16:34 +0200

    Moved Units and color grouping to the bottom navigation

--- frontend/app/src/main/java/com/example/smartstorageorganizer/HomeActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/HomeActivity.java
@@ -168,7 +168,7 @@ public class HomeActivity extends BaseActivity  {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_profile_management, R.id.nav_notifications, R.id.nav_help)
+                R.id.nav_home, R.id.nav_profile_management, R.id.nav_notifications, R.id.nav_help, R.id.nav_grouping, R.id.nav_units)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
