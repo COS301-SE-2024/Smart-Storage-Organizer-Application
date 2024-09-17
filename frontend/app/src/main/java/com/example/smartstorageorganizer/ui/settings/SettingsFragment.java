756ed079c3e0c71da68b902c420651aa8fbd0363
Mosa Letswalo <letswalomosa@tuks.co.za>
2024-09-17 17:05:52 +0200

    updated settings frag based on GLSurfaceView

--- frontend/app/src/main/java/com/example/smartstorageorganizer/ui/settings/SettingsFragment.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/ui/settings/SettingsFragment.java
@@ -1,17 +1,18 @@
 package com.example.smartstorageorganizer.ui.settings;
 
 import android.content.Intent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
-
 import androidx.fragment.app.Fragment;
-import com.example.smartstorageorganizer.DesignActivity;
+
+import com.example.smartstorageorganizer.MyGLRenderer;
+import com.example.smartstorageorganizer.MyGLSurfaceView;
 import com.example.smartstorageorganizer.ProfileManagementActivity;
 import com.example.smartstorageorganizer.R;
 import com.example.smartstorageorganizer.ReportsActivity;
@@ -19,6 +20,8 @@ import com.example.smartstorageorganizer.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
+    private GLSurfaceView glSurfaceView;
+    private MyGLRenderer renderer;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -27,8 +30,22 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Initialize GLSurfaceView
+//        glSurfaceView = new MyGLSurfaceView(getActivity());
+//
+
+        glSurfaceView = root.findViewById(R.id.glSurfaceView);
+        renderer = new MyGLRenderer();
+        glSurfaceView.setEGLContextClientVersion(2); // Use OpenGL ES 2.0
+        glSurfaceView.setRenderer(renderer);
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        // Add GLSurfaceView to the layout
+//        container.addView(glSurfaceView);
+
         Button profile = root.findViewById(R.id.profile);
         Button color = root.findViewById(R.id.color);
+        Button renderButton = root.findViewById(R.id.render_button);
 
         profile.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), ProfileManagementActivity.class);
@@ -40,6 +57,10 @@ public class SettingsFragment extends Fragment {
             startActivity(intent);
         });
 
+        renderButton.setOnClickListener(v -> {
+            // Trigger rendering of the 3D model
+            glSurfaceView.requestRender();
+        });
         return root;
     }
 
