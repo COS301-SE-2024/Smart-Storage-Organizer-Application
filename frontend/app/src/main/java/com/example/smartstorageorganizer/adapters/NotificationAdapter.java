9c7038c36ca59866e7fe3c0c5d1d28fc05c9889b
Segadimane-Makau <161035237+TshegofatsoMapheto@users.noreply.github.com>
2024-09-27 09:49:15 +0200

    Still fixing notifications

---
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/adapters/NotificationAdapter.java
@@ -0,0 +1,47 @@
+package com.example.smartstorageorganizer.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.smartstorageorganizer.R;
+import com.example.smartstorageorganizer.model.AppNotification;
+
+import java.util.List;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
+
+    private final List<AppNotification> notifications;
+
+    public NotificationAdapter(List<AppNotification> notifications) {
+        this.notifications = notifications;
+    }
+
+    @NonNull
+    @Override
+    public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_notifications, parent, false);
+        return new NotificationViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotificationViewHolder holder, int position) {
+        holder.notificationTextView.setText((CharSequence) notifications.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notifications.size();
+    }
+
+    static class NotificationViewHolder extends RecyclerView.ViewHolder {
+        TextView notificationTextView;
+
+        NotificationViewHolder(@NonNull View itemView) {
+            super(itemView);
+            notificationTextView = itemView.findViewById(R.id.text_notifications);
+        }
+    }
+}
