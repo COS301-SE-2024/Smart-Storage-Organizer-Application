02c35b64a6c40dbe2105da197a338570f3f61562
Mosa Letswalo <letswalomosa@tuks.co.za>
2024-09-29 21:20:20 +0200

    added member variables read/unread

--- frontend/app/src/main/java/com/example/smartstorageorganizer/NotificationModel.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/NotificationModel.java
@@ -4,11 +4,13 @@ public class NotificationModel {
     private String title;
     private String message;
     private String date;
+    private boolean isRead;
 
-    public NotificationModel(String title, String message, String date) {
+    public NotificationModel(String title, String message, String date, boolean isRead) {
         this.title = title;
         this.message = message;
         this.date = date;
+        this.isRead = isRead;
     }
 
     public String getTitle() {
@@ -21,5 +23,13 @@ public class NotificationModel {
 
     public String getDate() {
         return date;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
     }
 }
