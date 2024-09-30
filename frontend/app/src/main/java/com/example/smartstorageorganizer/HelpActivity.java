5d3e5f2a73ace3ff733f157f26c6a9ba92e36b19
Tshegofatso Mapheto <u20735929@tuks.co.za>
2024-09-30 21:42:23 +0200

    Code Scanner Help implemented

--- frontend/app/src/main/java/com/example/smartstorageorganizer/HelpActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/HelpActivity.java
@@ -17,10 +17,11 @@ public class HelpActivity extends BaseActivity {
         setContentView(R.layout.activity_help);
 
         setupHelpLinks();
+        findViewById(R.id.backButton).setOnClickListener(v -> finish());
     }
 
     private void setupHelpLinks() {
-        TextView help1 = findViewById(R.id.help_reset_password_text_view);
+        TextView help1 = findViewById(R.id.help_scan_item);
         TextView help2 = findViewById(R.id.help_add_category_text_view);
         TextView help3 = findViewById(R.id.help_edit_profile_text_view);
         TextView help4 = findViewById(R.id.help_unit_view);
@@ -29,7 +30,7 @@ public class HelpActivity extends BaseActivity {
         TextView help7 = findViewById(R.id.help_color_group_view);
         TextView help8 = findViewById(R.id.help_item_view);
 
-        makeLink(help1, "Forgot Password", new Intent(this, ResetPasswordActivity.class));
+        makeLink(help1, "Scan", new Intent(this, CodeScannerActivity.class));
         makeLink(help2, "Homepage", new Intent(this, HomeActivity.class));
         makeLink(help3, "EDIT PROFILE", new Intent(this, ProfileManagementActivity.class));
         makeLink(help4, "Unit", new Intent(this, UnitActivity.class));
