ea763e91d0684340e097b3b50371292456c9a3dc
Tshegofatso Mapheto <u20735929@tuks.co.za>
2024-09-30 19:30:31 +0200

    Generate Activity Help function done

--- frontend/app/src/main/java/com/example/smartstorageorganizer/HelpActivity.java
+++ frontend/app/src/main/java/com/example/smartstorageorganizer/HelpActivity.java
@@ -24,11 +24,13 @@ public class HelpActivity extends BaseActivity {
         TextView help2 = findViewById(R.id.help_add_category_text_view);
         TextView help3 = findViewById(R.id.help_edit_profile_text_view);
         TextView help4 = findViewById(R.id.help_unit_view);
+        TextView help5 = findViewById(R.id.help_generate_reports);
 
         makeLink(help1, "Forgot Password", new Intent(this, ResetPasswordActivity.class));
         makeLink(help2, "Homepage", new Intent(this, HomeActivity.class));
         makeLink(help3, "EDIT PROFILE", new Intent(this, ProfileManagementActivity.class));
         makeLink(help4, "Unit", new Intent(this, UnitActivity.class));
+        makeLink(help5, "Generate Report", new Intent(this, ReportsActivity.class));
     }
 
     private void makeLink(TextView textView, String linkText, Intent intent) {
